8a794394b4bed255470c332ec6f96064561e790c
Yuki Kondo <yuki.kondo@o-06964-mac.local>
2017-04-07 02:18:19 +0900

    add vim-endwise.git

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 git clone https://github.com/tomasr/molokai ~/.vim/
 
 curl --create-dirs -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+cd ~/.vim/bundle
+git clone git://github.com/tpope/vim-endwise.git
 
 gem install rsense
 apm install autocomplete-ruby
