398662c41b95cdefdd114a04cbe52d610ebef2f6
yuki-kondo <rexit.org@gmail.com>
2019-02-12 02:13:53 +0900

    fix

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
 git clone https://github.com/tomasr/molokai ~/.vim/
 
 curl --create-dirs -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+mkdir -p ~/.vim/bundle
+
 cd ~/.vim/bundle
 git clone git://github.com/tpope/vim-endwise.git
 
@@ -9,3 +12,4 @@ apm install autocomplete-ruby
 
 cd ~/.vim/bundle
 git clone git://github.com/tpope/vim-endwise.git
+
